020f3bfdd47c01975d1e52c0385215877dbcc002
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-01-20 18:00:40 +0600

    Use connect-once

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-module.exports = process.env.EXPRESS_MIDDLEWARE_COV
-  ? require('./lib-cov')
-  : require('./lib');
+/* jshint node:true */
+
+'use strict';
+
+var argv = require('optimist').argv;
+var cocaine = require('cocaine');
+var cli = new cocaine.Client(argv.locator);
+
+var connectOnce = require('connect-once');
+
+module.exports = function (services, options) {
+    options = options || {};
+
+    if (typeof services === 'string') {
+        services = [ services ];
+    }
+
+    var modules = connectOnce(cli.getServices, services);
+
+    return function expressCocainedService(req, res, next) {
+        modules.when('available', function () {
+            var args = Array.prototype.slice.apply(arguments);
+            if (args[0]) { return next(args[0]); }
+
+            for (var i = 0; i < services.length; i++) {
+                req[services[i]] = arguments[i + 1];
+            }
+        });
+    };
+};
---
+++ test/cocaine-service-test.js
@@ -0,0 +1,9 @@
+/* global describe, it */
+/* jshint expr:true */
+'use strict';
+
+describe('express-cocaine-service', function () {
+    it('should requires', function () {
+        require('..');
+    });
+});
